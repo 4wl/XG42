9aba9914fbb77c9dfb3c370c41f958ecbcb509fa
4wl <69080903+4wl@users.noreply.github.com>
2021-09-25 15:36:00 +0800

    delete smt

--- autobuild.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:copy
-start /B /WAIT gradlew.bat build
-copy /y "%CD%\build\libs\*-release.jar" "C:\Users\PyWong921\Desktop\test\.minecraft\mods"
-xcopy /y "%CD%\build\libs\*-release.jar" "D:\Minecraft Test Mod\mods"
-pause
-goto copy
